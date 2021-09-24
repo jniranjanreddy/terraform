0fc9b00dd5f31047be7927baacababda32fc54ac
niru <you@example.com>
2021-09-24 11:15:06 -0400

    add

---
+++ aws/cloud-watch-install/main.tf
@@ -0,0 +1,43 @@
+
+resource "aws_instance" "app" {
+  #count = 2
+  instance_type = var.instance
+  key_name      = "kube-demo"
+  ami           = var.image
+  user_data     = var.user_data
+  tags = {
+    #Name = "server-${count.index}"
+    Name = "server-01"
+  }
+}
+
+/*
+resource "aws_security_group" "aws_sg" {
+  #name        = "aws_sg"
+  #description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+*/
+
+data "aws_ec2_instance_type_offerings" "t2" {
+  location_type = "region"
+
+  filter {
+    name   = "instance-type"
+    values = ["t2.*"]
+
+  }
+}
