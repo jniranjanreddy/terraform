7e20db436e7cdb0820b070ea376c90a24d5cc074
niru <you@example.com>
2021-11-05 21:39:44 -0400

    lk

---
+++ aws/rhel7-k8s/provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  #region                  = "ap-south-1" # Arguments #
+  #region                  = "us-east-2" # Arguments #
+  region                   = "ap-south-1"
+  shared_credentials_file = "/root/.aws/credentials"
+  #  profile                    = "pocaws"
+}
