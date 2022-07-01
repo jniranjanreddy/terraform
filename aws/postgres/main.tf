1f25a1e281a8a68d8e03d214f9d3032dbdd1f40a
SG-Niranjan-Jannapureddy <you@example.com>
2022-07-01 07:01:32 -0400

    lk

---
+++ aws/postgres/main.tf
@@ -0,0 +1,23 @@
+provider "random" {
+  # Configuration options
+}
+
+resource "random_password" "password" {
+  length = 20
+  special = false
+  override_special = "_%@"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  identifier           =  "dev-db"     
+  engine_version       = "13"
+  instance_class       = "db.t3.medium"
+  name                 = "ntweeklydb001"
+  username             = "dbadmin1"
+  password             = random_password.password.result
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+}
