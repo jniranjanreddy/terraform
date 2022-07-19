398f79a944ae6ee1c35412f1f9277da015b0e86c
SG-Niranjan-Jannapureddy <you@example.com>
2022-07-19 10:37:30 -0400

    lk

--- aws/ubuntu-az/main.tf
+++ aws/ubuntu-az/main.tf
@@ -3,8 +3,13 @@ resource "aws_instance" "app" {
   key_name      = var.KEYS[var.AWS_REGION]
   ami           = var.AMIS[var.AWS_REGION]
   user_data     = var.user_data
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "server"
   }
+}
+
+resource "aws_instance" "example123" {
+  ami = var.AMIS[var.AWS_REGION]
+  instance_type = var.instance
 }
